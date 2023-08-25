1d932e8ec077d62a5fc871b0e28fcfe45babc82f
Esdras Abreu <xstrikeesdraspb@gmail.com>
2023-08-25 03:49:05 -0300

    feat: Interface layer implementation

---
+++ app/Repositories/FileRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+interface FileRepositoryInterface
+{
+    public function __construct(Model $model);
+    public function store(array $data);
+    public function getList();
+    public function get($id);
+    public function update($id, array $data);
+    public function destroy($id);
+}
